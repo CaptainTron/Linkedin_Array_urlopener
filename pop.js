ebb31792879e43cdd8bf6a26e4e7469a31f7628c
Vaibhav Yadav <tron21vaibhav@gmail.com>
2022-11-24 22:16:55 +0530

    Added to master branch

---
+++ pop.js
@@ -0,0 +1,10 @@
+const btn  =  document.getElementById('here') 
+btn.addEventListener('click',function() {
+    getCurrentTab() ;  
+});
+async function getCurrentTab() {
+        let queryOptions = { active: true, lastFocusedWindow: true };
+        let [tab]  = await chrome.tabs.query(queryOptions);
+        document.getElementById('demo').innerHTML = tab.title;
+        return tab;
+}
