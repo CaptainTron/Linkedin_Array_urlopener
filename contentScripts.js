58c12566398a5a09c0020aee4251b8c78318103b
Vaibhav Yadav <tron21vaibhav@gmail.com>
2022-12-03 02:41:40 +0530

    Successfully task completed and we are ready to go

--- contentScripts.js
+++ contentScripts.js
@@ -1,13 +1,36 @@
 (()  => {
-                // this will get the name and mail address of profile and put it on console
-                let nameinfo = document.getElementById("app-container").querySelector('section .member-description .member-name span').innerText;
-                let mailinfo = document.getElementById("app-container").querySelector('.profile-overview-container .contacts-container ul li div a') .innerText;
-                // console here
-                console.log(nameinfo);
-                console.log(mailinfo);
-                // talking to the popup.html and sending the data to pop;
-                // Ex. name of profile and Mailaddress of the profile. 
-                chrome.runtime.sendMessage({
-                        data: nameinfo,mailinfo
-                });  
+        let likec,commentc,totalcount;
+        chrome.runtime.sendMessage({
+            data: "DataFectched Successfully"
+             }, function (response) {
+              console.log(response);
+              likec = parseInt(response.data);
+              commentc = parseInt(response.data1)
+              totalcount =  likec + commentc
+              console.log(totalcount);
+            //   alert(totalcount);
+            
+        })
+        let i=0,k=0;
+                    function changehere() {
+                            if(i<likec){
+                                let like = document.querySelector('.scaffold-finite-scroll__content').querySelectorAll('div .feed-shared-social-action-bar__action-button .react-button__trigger')[i]
+                                like.click();
+                                i++;
+                            }
+                                if(k<commentc){
+                                    let commentbtn = document.querySelector('.scaffold-finite-scroll__content').querySelectorAll('div .comment span div button')[k]
+                                    commentbtn.click()
+                                    let comment = document.querySelector('.scaffold-finite-scroll__content').querySelectorAll('div .feed-shared-update-v2__comments-container .comments-comment-box .comments-comment-box__form-container .comments-comment-texteditor .comments-comment-box-comment__text-editor .editor-container .editor-content .ql-editor p')[k];
+                                    comment.innerHTML="CFBR";
+                                    let post = document.querySelector('.scaffold-finite-scroll__content').querySelectorAll('div .social-details-social-activity .feed-shared-update-v2__comments-container .comments-comment-box .comments-comment-box__form-container form')[k].querySelectorAll('div button')[2]
+                                    post.click();
+                                    k++;
+                                }
+                        }
+                    setInterval(changehere, 4000);
+                
 })();
+
+
+        
--- pop.js
+++ pop.js
@@ -18,13 +18,15 @@ comment.addEventListener('keyup',function(){
 })
 go.addEventListener('click',function(){
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                var tab = tabs[0]; 
-                // this will open the tabs here;
-                chrome.tabs.update(tab.id, {url: 'https://www.linkedin.com/feed/'});
-})
-})
-//     // This will fetch the data from the content_scripts file and display it here
-//     chrome.runtime.onMessage.addListener(function (message) {
-//         // getting data from the contentScript.js and putting it on popup.html
-         
-//     });    
+        var tab = tabs[0]; 
+        // this will open the tabs here;
+        chrome.tabs.update(tab.id, {url: 'https://www.linkedin.com/feed/'});
+        })
+        chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+            console.log(message)
+            sendResponse({
+                data: like.value,
+                data1: comment.value
+            }); 
+        });
+    })
